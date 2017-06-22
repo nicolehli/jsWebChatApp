be15d2d8d518e4075070eb476110ee4b1124a31d
nicolehli <nicolehcli@gmail.com>
2017-06-21 21:33:13 -0700

    App shows chat history for new user, but need to fix reload of chat history to existing user in chatroom :(

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /* testing */
 // console.log('hello from our node script!');
 
-/* loading external modules */
+/* Basic express server set up - loading external modules */
 var express = require('express');
 var app = express();
 var chatHistory = [];
@@ -9,13 +9,23 @@ var chatHistory = [];
 var http = require('http');
 var server = http.Server(app);
 
-/* server all static content from the client folder */
+/* Chatroom */
+var numUsers = 0;
+
+/* Routing - server all static content from the client folder */
 app.use(express.static('client'));
 
 /* using socket.io for bidirectional communicating */
 var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
+
+  if (chatHistory.length != 0) {
+    for(var i = 0; i < chatHistory.length; i++)
+        io.emit('message', chatHistory[i]);
+  }
+
+  // tell client we have new messages, this listens and executes
   socket.on('message', function (msg) {
     io.emit('message', msg);
     chatHistory.push(msg);
